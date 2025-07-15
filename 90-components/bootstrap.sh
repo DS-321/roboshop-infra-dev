c5cf79d8ea0ea9c8df624a853aeca8110181541f
SharmilaD <sharmiladaram419@gmail.com>
2025-07-15 16:30:29 +0530

    repo update

--- 90-components/bootstrap.sh
+++ 90-components/bootstrap.sh
@@ -3,4 +3,3 @@
 component=$1
 dnf install ansible -y
 ansible-pull -U https://github.com/DS-321/ansible-roboshop-roles-tf.git -e component=$1 -e env=$2 main.yaml
-#ansible-pull -U https://github.com/daws-84s/ansible-roboshop-roles-tf.git -e component=$1 -e env=$2 main.yaml
