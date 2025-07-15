e11b12f79a6f0a29822ffd7aca99881323a3ee20
SharmilaD <sharmiladaram419@gmail.com>
2025-07-15 17:29:55 +0530

    repo update

--- 90-components/variables.tf
+++ 90-components/variables.tf
@@ -13,9 +13,9 @@ variable "components" {
         shipping = {
             rule_priority = 40
         }
-        payment = {
-            rule_priority = 50
-        }
+        # payment = {
+        #     rule_priority = 50
+        # }
         frontend = {
             rule_priority = 10
         }
